e9e14bff44316913e6d1b4f1e4965359308e6c26
Vishakha261 <163869491+Vishakha261@users.noreply.github.com>
2024-06-13 16:04:10 +0530

    Update rng.py

--- rng.py
+++ rng.py
@@ -4,7 +4,7 @@ min_number = 143
 max_number = 300
 
 if (max_number < min_number): 
-  print('Invalid input - shutting down...'
+  print('Invalid input - shutting down...')
 else:
   rnd_number = randint(min_number, max_number)
   print(rnd_number)
